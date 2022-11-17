a8c5d5e81ab88f17ee716e4364cfeca5a3241af8
Magry Valentin <46904617+ValMgr@users.noreply.github.com>
2022-11-17 14:56:34 +0100

    [FEATURE] Get question from server ❓ (#13)
    * [FEAT] Create socket session & room management
    
    * [PACKAGE] Lock packages's versions
    
    * [FIX] Refacto & optimize RoomSelector
    
    * [CLEAN] Prettify files
    
    * [FEAT] Get question in order from server'
    
    * [CLEAN] Fix import path and remove unused ressource

--- app/src/modules/homePage/HomePage.tsx
+++ app/src/modules/homePage/HomePage.tsx
@@ -1,47 +1,31 @@
-import { useState, useCallback, ChangeEvent } from 'react';
+import { useMemo } from 'react';
 
 import { useAppContext } from '@/core/providers/AppProvider';
-import RoomSelector from './components/RoomSelector';
-import Questions from '../questions/Questions';
-import Gauges from '../gauges/Gauges';
+import GameProvider from '@/core/providers/GameProvider';
+import RoomSelector from '@/modules/homepage/components/RoomSelector';
+import Questions from '@/modules/questions/Questions';
+import Gauges from '@/modules/gauges/Gauges';
 import { MainContainer } from './styledComponents';
 
 export function HomePage() {
-	const [roomId, setRoomId] = useState<string>('');
-  const { setRoom, name, setName, room } = useAppContext();
+  const { room, name } = useAppContext();
 
-	const handleJoinRoom = useCallback(() => {
-		if(roomId && name) {
-			setRoom(roomId);
-		}
-  }, [setRoom, roomId]);
-  
-  const handleChangeRoomId = (e: ChangeEvent<HTMLInputElement>) => {
-    setRoomId(e.target.value);
-  }
+  const isRoomCreated = useMemo(() => name && room, [name, room]);
 
-  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  }
-
-	return (
-		<>
-		{name && room ? (
-			<MainContainer>
-				<Gauges />
-				<Questions />
-			</MainContainer>
-			) : (
-			<RoomSelector
-				onNameChange={handleChangeName}
-				onRoomIdChange={handleChangeRoomId}
-				onJoinRoom={handleJoinRoom}
-				roomIdValue={roomId}
-				nameValue={name}
-			/>
-		)}
-		</>
-	)
+  return (
+    <>
+      {isRoomCreated ? (
+        <GameProvider>
+          <MainContainer>
+            <Gauges />
+            <Questions />
+          </MainContainer>
+        </GameProvider>
+      ) : (
+        <RoomSelector />
+      )}
+    </>
+  );
 }
 
 export default HomePage;
--- app/src/modules/questions/Questions.tsx
+++ app/src/modules/questions/Questions.tsx
@@ -1,35 +1,21 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react';
 
-import Button from "./components/Button";
-import Timer from "./components/Timer";
-import { QuestionTitle, QuestionText, ContainerButtons,ContainerQuestion } from "@/modules/questions/components/styledComponents";
-import type { Question } from "@/types/question";
-
-import { useGameContext } from "@/core/providers/GameProvider";
-
-import getQuestion from "./services/api";
+import Button from '@/modules/questions/components/Button';
+import Timer from '@/modules/questions/components/Timer';
+import {
+  QuestionTitle,
+  QuestionText,
+  ContainerButtons,
+  ContainerQuestion,
+} from '@/modules/questions/components/styledComponents';
+import { useGameContext } from '@/core/providers/GameProvider';
 
 function QuestionContainer() {
-
-  const {updateScore} = useGameContext();
-  
-  const [question, setQuestion] = useState<Question | null>(null);
+  const { question, updateScore } = useGameContext();
   const [selected, setSelected] = useState<number>(-1);
 
-
-  useEffect(() => {
-    if(!question){
-     (async () => {
-      setQuestion(await getQuestion('/src/json/question.json'));
-    })();
-  }
-  }, [question]);
-
-
-
-
   const renderQuestionText: JSX.Element | null = useMemo(() => {
-    if(!question) {
+    if (!question) {
       return null;
     }
 
@@ -38,43 +24,38 @@ function QuestionContainer() {
         <QuestionTitle>{question.title}</QuestionTitle>
         <QuestionText>{question.question}</QuestionText>
       </>
-    )
+    );
   }, [question]);
 
   const renderAnswers: JSX.Element[] | null = useMemo(() => {
-    if(!question) {
+    if (!question) {
       return null;
     }
 
-    const handleClick = (index: number, answer:Object) => {
+    const handleClick = (index: number, answer: Object) => {
       setSelected(index);
       updateScore(answer);
 
       // @TODO: Send vote to server
     };
 
-
     return question.answers.map((answer, index) => (
-    <Button 
-      key={`answer_${answer.id}`} 
-      label={answer.text} 
-      callback={() => handleClick(index, answer.gauges)}
-      selected={selected === index} />
+      <Button
+        key={`answer_${answer.id}`}
+        label={answer.text}
+        callback={() => handleClick(index, answer.gauges)}
+        selected={selected === index}
+      />
     ));
   }, [question, selected]);
 
-
-
-  return(
+  return (
     <ContainerQuestion>
-      <Timer  />
+      <Timer />
       {renderQuestionText}
-      <ContainerButtons>
-        {renderAnswers}
-      </ContainerButtons>
+      <ContainerButtons>{renderAnswers}</ContainerButtons>
     </ContainerQuestion>
-  )
-
+  );
 }
 
 export default QuestionContainer;
