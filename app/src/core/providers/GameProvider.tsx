3adb57190fefc0fe53b4f255d9e6162b7a55d4e6
Valentin Magry <valou32720@gmail.com>
2022-11-17 10:24:28 +0100

    [FEAT] Manage min & max for gauges

--- app/src/core/providers/GameProvider.tsx
+++ app/src/core/providers/GameProvider.tsx
@@ -3,70 +3,73 @@ import { useState, useContext, createContext, useMemo } from 'react';
 import { GAUGE_MAX } from '@/core/constants/gauges';
 
 export type scoreType = {
-	social: number;
-	environmental: number;
-	economy: number;
-}
-
-export type GameContextType = {
-	users: string[];
-	setUsers: (users: string[]) => void;
-	score: scoreType;
-	updateScore: (modifier: {social?: number, environmental?: number, economy?: number}) => void;
+  environmental: number;
+  social: number;
+  economy: number;
 };
 
+export type GameContextType = {
+  users: string[];
+  setUsers: (users: string[]) => void;
+  score: scoreType;
+  updateScore: (modifier: { environmental?: number; social?: number; economy?: number }) => void;
+};
 
 const GameContext = createContext<GameContextType>({
-	users: [],
-	setUsers: () => {},
-	score: {
-		social: 0,
-		environmental: 0,
-		economy: 0,
-	},
-	updateScore: () => {},
+  users: [],
+  setUsers: () => {},
+  score: {
+    environmental: 0,
+    social: 0,
+    economy: 0,
+  },
+  updateScore: () => {},
 });
 
 interface IProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const GameProvider = ({ children }: IProps) => {
   const [users, setUsers] = useState<string[]>([]);
   const [progression, setProgression] = useState<number>(0);
-	const [score, setScore] = useState<scoreType>({ social: 0, environmental: 0, economy: 0 });
+  const [score, setScore] = useState<scoreType>({ environmental: 0, social: 0, economy: 0 });
 
-	// function checkLimit(value: number, score: number) {
-	// 	if (value >= GAUGE_MAX) return GAUGE_MAX;
-	// 	if (value <= -GAUGE_MAX) return -GAUGE_MAX;
-	// 	return value;
-	// }
+  const updateScore = (modifier: { environmental?: number; social?: number; economy?: number }) => {
+    setScore((score) => {
+      return {
+        environmental: getValue(score.environmental, modifier.environmental || 0),
+        social: getValue(score.social, modifier.social || 0),
+        economy: getValue(score.economy, modifier.economy || 0),
+      };
+    });
+  };
 
-	const updateScore = (modifier: { social?: number, environmental?: number, economy?: number }) => {
-		// @TODO: check if score is between - GAUGE_MAX and GAUGE_MAX before updating
-		
-		setScore((score) => ({
-			social: score.social + (modifier.social || 0),
-			environmental: score.environmental + (modifier.environmental || 0),
-			economy: score.economy + (modifier.economy || 0),
-		}));
-	}
-
+  const getValue = (v: number, m: number) => {
+    if (v + m > GAUGE_MAX) {
+      return GAUGE_MAX;
+    }
+    if (v + m < -GAUGE_MAX) {
+      return -GAUGE_MAX;
+    }
+    
+    return v + m;
+  };
 
   const value = useMemo(() => {
     return {
-			users,
-			setUsers,
+      users,
+      setUsers,
       progression,
-			setProgression,
-			score,
-			updateScore
-    }
+      setProgression,
+      score,
+      updateScore,
+    };
   }, [users, setUsers, progression, setProgression, score, setScore]);
 
-	return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
-}
+  return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
+};
 
-export const useGameContext = () => useContext(GameContext)
+export const useGameContext = () => useContext(GameContext);
 
-export default {GameProvider, useGameContext};
+export default { GameProvider, useGameContext };
