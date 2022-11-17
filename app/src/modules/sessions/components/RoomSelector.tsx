92f4835bef7217e7902cff1f44d7dd6ca22c2fef
Thomas-Af <thomas.afonso@mail-ecv.fr>
2022-11-17 14:51:28 +0100

    [feat] : add screen results

---
+++ app/src/modules/sessions/components/RoomSelector.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, useCallback, useState } from 'react';
+import { useAppContext } from '@/core/providers/AppProvider';
+
+function RoomSelector() {
+  const [roomId, setRoomId] = useState<string>('');
+  const { setRoom, name, setName } = useAppContext();
+
+  const handleJoinRoom = useCallback(() => {
+    setRoom(roomId);
+  }, [setRoom, roomId]);
+
+  const handleChangeRoomId = (e: ChangeEvent<HTMLInputElement>) => {
+    setRoomId(e.target.value);
+  }
+
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
+
+  return (
+    <div className={'d-flex justify-content-center w-100'}>
+      <input type='text' name='name' placeholder='Username' onChange={handleChangeName} value={name} />
+      <input type='text' name='room_id' placeholder='room ID' onChange={handleChangeRoomId} value={roomId} />
+      <button onClick={handleJoinRoom} type='submit'>Join</button>
+    </div>
+  );
+}
+
+export default RoomSelector;
